1af105f99881811fdff550f1e7f98e54cc267392
fabrazzak <freelancerabdurrazzak47@gmail.com>
2025-01-20 19:20:44 +0600

    update code

--- src/pages/Admin/Statistics/Statistics.jsx
+++ src/pages/Admin/Statistics/Statistics.jsx
@@ -4,6 +4,13 @@ const Statistics = () => {
     return (
         <div>
             <h2>this is Statistics page </h2>
+
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="bar"
+              width="500"
+            />
         </div>
     );
 };
