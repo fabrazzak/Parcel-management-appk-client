928c74c4056049151ff6dee185b26cc60a43956c
fabrazzak <freelancerabdurrazzak47@gmail.com>
2025-01-21 19:24:10 +0600

    update

--- src/hooks/useAxiosSecures.jsx
+++ src/hooks/useAxiosSecures.jsx
@@ -3,13 +3,15 @@ import { useEffect } from "react";
 
 const useAxiosSecures = (navigate) => {
   const instance = axios.create({
-    // baseURL:`${import.meta.env.VITE_LIVE_UR}/`
-    baseURL: 'https://percel-management-app-server.vercel.app/',
+    baseURL:`${import.meta.env.VITE_LIVE_UR}/`
+    // baseURL: 'https://percel-management-app-server.vercel.app/',
   });
 
   useEffect(() => {
     // Request Interceptor
-    const requestInterceptor = instance.interceptors.request.use(
+
+    
+    instance.interceptors.request.use(
       (config) => {
         const token = localStorage.getItem("token");
         if (token) {
@@ -20,8 +22,8 @@ const useAxiosSecures = (navigate) => {
       (error) => Promise.reject(error)
     );
 
-    // Response Interceptor
-    const responseInterceptor = instance.interceptors.response.use(
+
+    instance.interceptors.response.use(
       (response) => response,
       async (error) => {
         const status = error?.response?.status;
@@ -31,13 +33,8 @@ const useAxiosSecures = (navigate) => {
         return Promise.reject(error);
       }
     );
-
-    // Cleanup interceptors on unmount
-    return () => {
-      instance.interceptors.request.eject(requestInterceptor);
-      instance.interceptors.response.eject(responseInterceptor);
-    };
-  }, [navigate]);
+   
+  }, []);
 
   return instance;
 };
